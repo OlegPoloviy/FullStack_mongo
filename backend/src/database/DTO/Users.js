b9160bca172f0ec02c9d96a039e07df70e5ea327
Oleg Poloviy <125209908+OlegPoloviy@users.noreply.github.com>
2024-11-09 17:25:49 +0200

    changed db

--- backend/src/database/DTO/Users.js
+++ backend/src/database/DTO/Users.js
@@ -1,13 +1,16 @@
-import { connection } from "../connection.js";
+import { connectToDatabase } from "../connection.js";
 
 export class Users {
   static async getAll() {
-    const users = await connection();
+    const users = await connectToDatabase();
 
     return users.map(user => ({
-      id: user._id, 
+      id: user._id,
       name: user.name,
       email: user.email,
     }));
+  }
+
+  static async addUser(data) {
   }
 }
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,27 +1,15 @@
-// connection.js
-import { MongoClient } from "mongodb";
-import "dotenv/config";
+import mongoose from "mongoose"
+import "dotenv/config"
 
-const uri = process.env.DB_URL;
-const client = new MongoClient(uri);
+const url = process.env.DB_URL;
 
-export async function connection() {
-  try {
-    await client.connect();
-    console.log("Підключено до MongoDB");
-
-    // Підключення до бази даних і колекції
-    const database = client.db("sample_mflix");
-    const collection = database.collection("users");
-
-    // Отримання всіх документів у колекції 'users'
-    const users = await collection.find({}).toArray();
-    return users;
-  } catch (error) {
-    console.error("Помилка підключення:", error);
-    return [];
-  } finally {
-    // Закриття з'єднання після виконання запиту
-    await client.close();
+async function connectToDatabase(){
+  try{
+    await mongoose.connect(url);
+    console.log("Connected");
+  }catch(err){
+    console.error(err);
   }
 }
+
+export const userSchema = new mongoose.Schema
